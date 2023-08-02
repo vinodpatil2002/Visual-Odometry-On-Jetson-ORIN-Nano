6eecf312bad39531c415044574515cb9657fd4f0
Vinod N Patil <83590514+vinodpatil2002@users.noreply.github.com>
2023-08-02 11:37:56 +0530

    Add files via upload

---
+++ ORB_SLAM2/Examples/Monocular/lenovo.cc
@@ -0,0 +1,100 @@
+/**
+* This file is part of ORB-SLAM2.
+*
+* Copyright (C) 2014-2016 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
+* For more information see <https://github.com/raulmur/ORB_SLAM2>
+*
+* ORB-SLAM2 is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* ORB-SLAM2 is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+#include<stdio.h>
+#include<chrono>
+#include<unistd.h>
+
+#include<opencv2/core/core.hpp>
+#include<System.h>
+
+using namespace std;
+
+void LoadImages(const string &strImagePath, const string &strPathTimes,
+                vector<string> &vstrImages, vector<double> &vTimeStamps);
+
+int main(int argc, char **argv)
+{
+    cv::VideoCapture cap(0); // Open the default camera (change the index if you have multiple cameras)
+
+    if (!cap.isOpened())
+    {
+        cerr << "Failed to open the camera" << endl;
+        return 1;
+    }
+
+    ORB_SLAM2::System SLAM("/home/vision/ORB_SLAM2/Vocabulary/ORBvoc.txt", "/home/vision/ORB_SLAM2/Examples/Monocular/lenovo.yaml", ORB_SLAM2::System::MONOCULAR, true);
+
+    // Main loop
+    cv::Mat im;
+    int counter = 0;
+    while (true)
+    {
+        cap >> im; // Capture a frame from the camera
+
+        if (im.empty())
+        {
+            cerr << endl << "Failed to load image" << endl;
+            return 1;
+        }
+
+        std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+
+        // Pass the image to the SLAM system
+        SLAM.TrackMonocular(im,++counter);
+
+        std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+
+        double ttrack = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1).count();
+    }
+
+    SLAM.Shutdown();
+    SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
+
+    return 0;
+}
+
+void LoadImages(const string &strImagePath, const string &strPathTimes,
+                vector<string> &vstrImages, vector<double> &vTimeStamps)
+{
+    ifstream fTimes;
+    fTimes.open(strPathTimes.c_str());
+    vTimeStamps.reserve(5000);
+    vstrImages.reserve(5000);
+    while (!fTimes.eof())
+    {
+        string s;
+        getline(fTimes, s);
+        if (!s.empty())
+        {
+            stringstream ss;
+            ss << s;
+            vstrImages.push_back(strImagePath + "/" + ss.str() + ".png");
+            double t;
+            ss >> t;
+            vTimeStamps.push_back(t / 1e9);
+        }
+    }
+}
+
